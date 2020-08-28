a979fda4d40364c23dfffda8a180e21b89ec3a42
Danait Aregay <danait@Danaits-MacBook-Pro.local>
2020-08-28 01:57:07 -0600

    added more files

---
+++ src/cleaning_eda.py
@@ -0,0 +1,254 @@
+# libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import rcParams
+import math
+
+def cleaning_df(df, col, value_greater, value_less):
+    '''uses the dataframe and column to locate rows that contain certain values and drops those rows'''
+    lst= ((df[df[col] > value_greater]).index).tolist()
+    df.drop(lst, axis=0, inplace= True)
+    lst2= ((df[df[col] < value_less]).index).tolist()
+    df.drop(lst2, axis=0, inplace= True)
+    return df
+
+
+def get_percentage(df, col, target_col):
+    '''given a dataframe and column name, it returns a new dataframe that groups by the column 
+    and percentage of the column values that are in the positive class for the classifier target variable'''
+    df2= df.groupby(col).sum()[target_col]*100/df[col].value_counts()
+    return df2.reset_index()
+
+
+def percent_barplot (df, col, xlabel, title, color= 'cmk'): 
+    '''returns barplot of percentage of certain catogorical columns that is in target class '''
+    label= ['normal','above normal','well above normal']
+    fig= plt.figure(figsize=(15,8))
+    plt.bar(label, df[col], color= color)
+    plt.ylabel('percent', size= 18)
+    plt.title(title, size= 22)
+    plt.xticks(size= 15)
+    plt.yticks(size= 15)
+    plt.xlabel(xlabel, size= 18)
+    plt.style.use('seaborn')
+    plt.show()
+    return plt.show()
+
+
+def year_count_barplot(df, col, x, hue):
+    '''takes dataframe, counts the number of postive target class for all unique values in a column'''
+    df[col] = (df['age'] / 365).round().astype('int')
+    data1= df.rename(columns={'cardio': 'CVD'})
+    data1['CVD'] = data1['CVD'].map({0: 'No', 1: 'Yes'})
+    # plot of counts 
+    plt.figure(figsize=(18,10))
+    sns.countplot(x=x, hue=hue, data = data1, palette="Set2")
+    plt.ylabel('Number of people', size= 22)
+    plt.title(label= "Count of People Who Have/Don't Have CVD for Each Age Group", size= 30)
+    plt.xticks(size= 22)
+    # plt.xlim(False)
+    plt.yticks(size= 15)
+    plt.xlabel('Age', size= 22)
+    plt.style.use('seaborn')
+    plt.xlim ()
+    plt.legend(fontsize= 22, title= 'CVD', loc= 2, title_fontsize= 22)
+    return None
+
+
+def multcolumn_groupby(df,col1, col2 ):
+    '''returns two dataframes, one groupedby two columns and counted, the other a new dataframe with col1
+    and how many times each of its values accur'''
+    percent= df.groupby([col1, col2]).count().reset_index()
+    total= df.groupby(col1).count()[col2]
+    total_df= pd.DataFrame(total.index, total).reset_index()
+    return percent, total_df
+
+
+def groupby_columns(col1, col2, df, name):
+    '''given dataframe, groups by col1 and counts by col2 and  returns new dataframe using the given name'''
+    df_= df.groupby('col1').count()[col2]
+    name= pd.DataFrame(df_.index, df).reset_index()
+    return name
+
+
+def create_dictionary (df, value_col, key_col):
+    '''given a dataframe, value_col and key_col are the colummns that will be used to create a 
+    dictionary with key_col as key and value_col as key'''
+    dic= {}
+    value= df[value_col].tolist()
+    key= df[key_col].tolist()
+    for k,v in zip (key, value):
+        dic[k]= v
+    return dic 
+
+
+def add_list_column(col, dic1, df):
+    '''for all values in a col given a dataframe, the function will see if the values are in a dictionary and 
+    return a list of all those values'''
+    lst= []
+    for i in df[col]:
+        for k, v in dic1.items():
+            if i == k:
+                lst.append(v)
+    return lst
+
+
+def add_column(col, dic1, df, col_name, column_perc, column):
+    ''' takes the function add_list_column which return a list, adds the returned list to a dataframe
+    and creates another list that takes percentage of two columns
+    df= dataframe
+    col, dic1,df = the column, dictionary and dataframe that will be passed to add_column
+    column= the column used to create percentage 
+    '''
+    df[col_name]= add_list_column(col, dic1, df)
+    df[column_perc]= df[column]*100/df[col_name]
+    return df
+
+
+def count_percent_barplot(cardio_):
+    "finds the percentage "
+    percent_0=  cardio_[cardio_['cardio']== 0]
+    percent0_lst= percent_0['total_percent'].tolist()
+    percent_1= cardio_[cardio_['cardio'] == 1]
+    percent1_lst= percent_1['total_percent'].tolist()
+    percent1_lst.insert(0,0)
+    indices = percent_0['years']
+    #plots the percentage
+    width = np.min(np.diff(indices))/3
+    fig = plt.figure(figsize= (20,12))
+    ax = fig.add_subplot(111)
+    ax.bar(indices-width,percent0_lst,width,color='b',label='no CVD')
+    ax.bar(indices,percent1_lst,width,color='r',label='CVD')
+    ax.set_xlabel('Age', size= 30)
+    ax.set_ylabel('Percent', size = 30)
+    ax.set_title("Percent of People Who have/ don't have CVD for each Age Group", size=30)
+    ax.legend(fontsize= 22)
+    ax.tick_params(axis='both', which='major', labelsize=22)
+    return None
+
+
+def lifestyle_df(df, rename_column, column, rename_values):
+    ''' it renames a columns in the dataframe=df, given dictionary called rename_column,
+    it also changes the catergorical value names of another column given dictionary called rename_values
+    returns dataframe '''
+    data2= df.rename(columns=rename_column)
+    data2['CVD'] = data2['CVD'].map({0: 'No', 1: 'Yes'})
+    data2[column]= data2[column].map(rename_values)
+    return data2
+
+
+def lifestyle_barplot(data, x, hue, xlabel, title):
+    '''plots a barplot given data as a dataframe, x= the column to plot
+    hue= color encoding column'''
+    plt.figure(figsize=(18,8))
+    sns.countplot(x=x, hue=hue, data=data, palette="Set2")
+    plt.ylabel('Number of people', size= 30)
+    plt.title(label= title, size= 30)
+    plt.xticks(size= 22)
+    # plt.xlim(False)
+    plt.yticks(size= 15)
+    plt.xlabel(xlabel, size= 22)
+    plt.style.use('seaborn')
+    plt.xlim ()
+    plt.legend(fontsize= 22, title= 'CVD', loc= 1, title_fontsize= 22)
+    return None
+
+
+def bmi_barplot(df):
+    '''returns a counted barplot of the BMI column'''
+    data = df.rename(columns={'cardio': 'CVD'})
+    data['CVD'] = data['CVD'].map({0: 'No', 1: 'Yes'})
+    plt.figure(figsize=(20,14))
+    plot_= sns.countplot(x='BMI', hue='CVD', data = data, palette="Set2")
+    plt.ylabel('Number of People', size= 22)
+    plt.title(label= 'Count of people who have CVD for each Body Mass Index', size= 30)
+    plt.xticks(size= 22)
+    plt.xlim()
+    plt.yticks(size= 15)
+    plt.xlabel('Body Mass Index (BMI range - kg/m2)', size= 22)
+    plt.style.use('seaborn')
+    plt.xlim ()
+    plt.legend(fontsize= 22, title= 'CVD', loc= 1, title_fontsize= 22)
+    #fig.canvas.draw()
+    new_ticks = [i.get_text() for i in plot_.get_xticklabels()]
+    plt.xticks(range(0, len(new_ticks), 10), new_ticks[::10])
+    return None
+
+
+def count_barplot(df, col,title, xlabel):
+    '''returns a counted barplot of the col values that are in target class'''
+    data = df.rename(columns={'cardio': 'CVD'})
+    data['CVD'] = data['CVD'].map({0: 'No', 1: 'Yes'})
+    # make countplot
+    plt.figure(figsize=(20,14))
+    plot_= sns.countplot(x=col, hue='CVD', data = data, palette="Set2")
+    plt.ylabel('Number of People', size= 22)
+    plt.title(label= title, size= 30)
+    plt.xticks(size= 22)
+    plt.xlim()
+    plt.yticks(size= 15)
+    plt.xlabel(xlabel, size= 22)
+    plt.style.use('seaborn')
+    plt.xlim()
+    plt.legend(fontsize= 22, title= 'CVD', loc= 1, title_fontsize= 22)
+    #fig.canvas.draw()
+    new_ticks = [i.get_text() for i in plot_.get_xticklabels()]
+    plt.xticks(range(0, len(new_ticks), 10), new_ticks[::10])
+    return None
+
+
+def create_dummies(df, column,add1, add2):
+    '''creates dummies variables using the column and adds them to the dataframe
+     delets the original column'''
+    dummy= pd.get_dummies(df[column], prefix= column)
+    df[ [ add1, add2] ] = dummy[ [add1, add2]]
+    del df[column]
+    return df 
+
+
+if __name__ == "__main__":
+    # import dataframe 
+    unclean_df= pd.read_csv('data/cardio_train.csv')
+    # clean the dataframe
+    unclean_df=cleaning_df(unclean_df, 'ap_lo', 200, 0)
+    cvd=cleaning_df(unclean_df, 'ap_hi', 300, 0)
+    # EDA
+    # take percentage of how many people have cvd for each glucose classifications and make barplot
+    gluc_df = get_percentage(cvd, 'gluc', 'cardio')
+    gluc_barplot= percent_barplot(gluc_df, 0, 'Glucose classification', 'Percent of People Who Have CVD for Each Glucose Classification')
+    # take percentage of how many people have cvd for each cholestrol classifications and make barplot
+    chole_df= get_percentage(cvd, 'cholesterol', 'cardio')
+    cholestrol_barplot=percent_barplot(chole_df, 0, 'Cholesterol Classification', 'Percent of People Who Have CVD for Each Cholesterol Classification')
+    # barplot of count of cvd as age increases 
+    year_count_barplot(cvd, 'years', 'years', 'CVD')
+    # makes percentage of how many peopl in each age group have cvd/dont have cvd and plots barplot
+    cardio_percent, cardio_total= multcolumn_groupby(cvd,'years', 'cardio' )
+    dic= create_dictionary(cardio_total, 'cardio', 'years')
+    cardio_ = add_column('years',dic,cardio_percent, 'total_cardio', 'total_percent', 'id')
+    count_percent_barplot(cardio_)
+    # cleans the column to make a plot and plots cvd amoung number of people who smoke/dont smoke
+    smoke= lifestyle_df(cvd, {'cardio': 'CVD'}, 'smoke', {0: "Don't Smoke", 1: 'Smoke'})
+    lifestyle_barplot(smoke, 'smoke', 'CVD', "People Who Smoke and Don't Smoke", "Smoking and Cardiovascular Disease (CVD)")
+    # cleans the column to make a plot and plots cvd amoung number of people who drink/dont drink alcohol
+    alcohol= lifestyle_df(cvd, {'cardio': 'CVD'}, 'alco', {0: "Don't Drink", 1: "Drink"})
+    lifestyle_barplot(alcohol, 'alco', 'CVD', 'Alcohol', "Alcohol Consumption and Cardiovascular Disease (CVD)")
+    # cleans the column to make a plot and plots cvd amoung number of people who drink/dont drink alcohol
+    excercise= lifestyle_df(cvd, {'cardio': 'CVD'}, 'active', {0: 'Not Active', 1: "Active"})
+    lifestyle_barplot(excercise, 'active', 'CVD', 'Excercise', "Excercise and Cardiovascular Disease (CVD)")
+    # barplot of count of people for each  BMI group who have cvd/ don't have cvd 
+    bmi_barplot(cvd)
+    # barplot of cvd and diastolic blood pressure and systolic blood pressure 
+    count_barplot(cvd,'ap_lo', "Diastolic blood pressure and Number of People who have/ don't have CVD",'Diastolic blood pressure (mmHg)')
+    count_barplot(cvd,'ap_hi', "Systolic blood pressure and Number of People who have/ don't have CVD",'Systolic blood pressure (mmHg)')
+    # further data engineering for model selection and save the final dataframe 
+    cvd['gender']= cvd['gender'].map({1: 0, 2: 1})
+    del cvd['height']
+    del cvd['weight']
+    del cvd ['id']
+    del cvd['years']
+    cvd= create_dummies(cvd, 'cholesterol','cholestrol_2','cholestrol_3')
+    cvd= create_dummies(cvd, 'gluc','gluc_2', 'gluc_3')
+    cvd.to_csv('final_df.csv')
+    pass
---
+++ src/cvd_model_selection.py
@@ -0,0 +1,181 @@
+# libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import rcParams
+import math
+# models
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+# train-test-split
+from sklearn.model_selection import train_test_split, KFold
+# hyperparameter tuning parameter search methodology
+from sklearn.model_selection import RandomizedSearchCV
+#  metrics
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, classification
+# other tools
+from statsmodels.tools import add_constant
+from scipy.stats import uniform
+from statsmodels.discrete.discrete_model import Logit
+from sklearn.preprocessing import StandardScaler
+
+def split_train_holdout(df, target, test_size=0.2, random_state=33):
+    '''takes dataframe specifies the feature variables and the target variable, 
+    divides the data into the training and holdout data using given test_size, and random_state
+    '''
+    y= df[target].values
+    del df[target]
+    x= df.values
+    X_train, X_holdout, y_train, y_holdout = train_test_split(x, y, 
+                                                          shuffle=True,
+                                                          test_size=test_size, 
+                                                          random_state=random_state)
+    return X_train, X_holdout, y_train, y_holdout 
+
+def gradient_boosting(X_train, y_train):
+    '''finds the best parameters for Gradient Boosting classifier using randomed search cv'''
+    # declare the classifier
+    gclassifier = GradientBoostingClassifier()
+    # parameters to investigate
+    num_estimators = [8, 15, 50, 100]
+    criterion = ['friedman_mse', 'mse', 'mae']
+    max_features = ['auto', 'sqrt', "log2"]
+    max_depth = [5, 10, None]
+    min_samples_split = [2, 10, 30]
+    min_samples_leaf = [1, 3]
+    # dictionary containing the parameters
+    param_grid = {'n_estimators': num_estimators,
+              'max_features': max_features,
+              'max_depth': max_depth,
+              'min_samples_split': min_samples_split,
+              'min_samples_leaf': min_samples_leaf,
+              'criterion': criterion}
+    # hyperparameter tuning parameter search methodology
+    desired_iterations = 100
+    g_random_search = RandomizedSearchCV(gclassifier, 
+                                   param_grid, 
+                                   scoring='recall',
+                                   cv=3,
+                                   n_iter=desired_iterations,
+                                   verbose=1,
+                                   return_train_score=True,
+                                   n_jobs=-1)
+    g_random_search.fit(X_train, y_train)
+    # print the bestparameters and the best recall score
+    gradientboosting_randomsearch_bestparams = g_random_search.best_params_
+    gradientboosting_randomsearch_bestscore =  g_random_search.best_score_
+    return gradientboosting_randomsearch_bestparams , gradientboosting_randomsearch_bestscore
+
+def random_forest(X_train, y_train):
+    '''finds the best parameters for Random Forest classifier using randomed search cv'''
+    # declare the classifier
+    classifier = RandomForestClassifier()
+    # parameters to investigate
+    num_estimators = [90,150,300,1000]
+    criterion = ["gini", 'entropy']
+    max_features = ['auto', 'sqrt', "log2"]
+    max_depth = [20, 10, None]
+    min_samples_split = [2, 3, 10]
+    min_samples_leaf = [3, 5,7]
+    # dictionary containing the parameters
+    param_grid = {'n_estimators': num_estimators,
+              'max_features': max_features,
+              'max_depth': max_depth,
+              'min_samples_split': min_samples_split,
+              'min_samples_leaf': min_samples_leaf,
+              'criterion': criterion}
+    # hyperparameter tuning parameter search methodology
+    desired_iterations = 100
+    random_search = RandomizedSearchCV(classifier, 
+                                   param_grid, 
+                                   scoring='recall',
+                                   cv=3,
+                                   n_iter=desired_iterations,
+                                   verbose=1,
+                                   return_train_score=True,
+                                   n_jobs=-1)
+    random_search.fit(X_train, y_train)
+    # print the bestparameters and the best recall score
+    randomforest_randomsearch_bestparams = random_search.best_params_
+    randomforest_randomsearch_bestscore =  random_search.best_score_
+    return randomforest_randomsearch_bestparams , randomforest_randomsearch_bestscore 
+
+def scaledata(df, num_col):
+    '''scales the number columns in the dataframe so that they have mean of 0 s
+    num_col= list of column names to be scaled'''
+    scaler= StandardScaler()
+    toscale= df[num_col].values
+    df.drop([num_col], axis=1, inplace= True)
+    scaler1= scaler.fit(toscale)
+    scaler2= scaler1.transform(toscale)
+    scalerdata=pd.DataFrame(scaler2, columns=num_col)
+    scalerdata.head()
+    df= pd.concat([df, scalerdata], axis=1)
+    return df
+
+
+def logistic_regression(X_train, y_train, solver=['liblinear']):
+    '''finds the best parameters for Logistic Regresssion classifier using randomed search cv
+    for a given solver as a list'''
+    # declare the classifier
+    logistic = LogisticRegression()
+    # parameters to investigate
+    if solver == ['lbfgs'] or solver == ['newton-cg'] or solver== ['sag']:
+        penalty = ['l2', 'none']
+    elif solver == ['liblinear']:
+        penalty = ['l1', 'l2']
+    elif solver == ['sage']:
+        penalty = ['elasticnet']
+    distributions = dict(C=uniform(loc=0, scale=4),
+                    penalty=penalty,
+                    max_iter= [100, 200, 500, 1000],
+                    solver= solver)
+    # hyperparameter tuning parameter search methodology
+    desired_iterations = 100
+    search = RandomizedSearchCV(logistic, 
+                                   distributions, 
+                                   scoring='recall',
+                                   cv=3,
+                                   n_iter=desired_iterations,
+                                   verbose=1,
+                                   return_train_score=True,
+                                   n_jobs=-1, 
+                                   random_state=0)
+    log_search = search.fit(X_train, y_train)
+    logistic_randomsearch_bestparams = log_search.best_params_
+    logistic_randomsearch_bestscore =  log_search.best_score_
+    return logistic_randomsearch_bestparams, logistic_randomsearch_bestscore
+
+def finalmodel(X_train, y_train, X_holdout, y_holdout,Classifier, **kwargs):
+    ''' given classifier and the best parameters (**kwargs) for that classifier, it returns the
+    predict, score, precision, and recall'''
+    final_model= Classifier(kwargs)
+    final_model.fit(X_train, y_train)
+    y_predict= final_model.predict(X_holdout)
+    score=final_model.score(X_holdout, y_holdout)
+    predict=final_model.predict(X_holdout)
+    precision=precision_score(y_holdout, y_predict)
+    recall=recall_score(y_holdout, y_predict)
+    confusion_matrix=confusion_matrix(y_holdout, y_predict)
+    dic={"score": score, "precision": precision, 'recall':recall, 'confusion_matrix': confusion_matrix}
+    return (f'for {Classifier} the final model with optimized hyperparameters results in {dic}')
+
+if __name__ == "__main__":
+    # import the dataframe and split the data
+    model_df= pd.read_csv('data/final_df.csv')
+    # model_df= df2.copy()
+    X_train, X_holdout, y_train, y_holdout= split_train_holdout(model_df, 'cardio')
+    # run randomsearchcv for gradientboosting and use the result bestparameters to create the scores for the final model
+    gradientboosting_randomsearch_bestparams,gradientboosting_randomsearch_bestscore = gradient_boosting(X_train, y_train)
+    gradientboosting_finalmodel= finalmodel(X_train, y_train, X_holdout, y_holdout,GradientBoostingClassifier,gradientboosting_randomsearch_bestparams)
+    # run randomsearchcv for randomforest and use the result bestparameters to create the scores for the final model
+    randomforest_randomsearch_bestparams , randomforest_randomsearch_bestscore = random_forest(X_train, y_train)
+    randomforest_finalmodel= finalmodel(X_train, y_train, X_holdout, y_holdout,RandomForestClassifier,randomforest_randomsearch_bestparams)
+    # scale data then run randomsearchcv for logistic regression and use the result bestparameters to create the scores for the final model
+    cvd= scaledata(cvd, ['age', 'ap_hi', 'ap_lo', 'BMI'])
+    X_trainn, X_test, y_trainn, y_test= split_train_holdout(cvd, 'cardio')
+    logistic_randomsearch_bestparams, logistic_randomsearch_bestscore= logistic_regression(X_trainn, y_trainn)
+    logisticregression_finalmodel=finalmodel(X_trainn, y_trainn, X_test, y_test,LogisticRegression, logistic_randomsearch_bestparams)
